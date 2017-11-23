b50707b108f2ce120d0e31294cdc828590c5b05e
hanisha <hanisha946@gmail.com>
2017-11-23 23:48:33 +0530

    Hanisha | #140 | Anonymising the patient names

---
+++ api/src/main/java/org/openmrs/module/anonymize/advice/PatientSaveInterceptor.java
@@ -0,0 +1,30 @@
+package org.openmrs.module.anonymize.advice;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.openmrs.Patient;
+import org.openmrs.PersonName;
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class PatientSaveInterceptor implements MethodBeforeAdvice {
+    private static final String SAVE_PATIENT_METHOD = "savePatient";
+    private static final String ANONYMISE = "Consent for anonymization";
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        if (method.getName().equals(SAVE_PATIENT_METHOD)) {
+            Patient patient = (Patient) args[0];
+            if(patient != null && patient.getAttribute(ANONYMISE) != null
+                    && patient.getAttribute(ANONYMISE).getValue().equals("true")) {
+                PersonName personName = patient.getPersonName();
+                if(personName != null) {
+                    personName.setFamilyName(RandomStringUtils.randomAlphabetic(5));
+                    personName.setGivenName(RandomStringUtils.randomAlphabetic(5));
+                    personName.setMiddleName(RandomStringUtils.randomAlphabetic(5));
+                }
+            }
+
+        }
+    }
+}
