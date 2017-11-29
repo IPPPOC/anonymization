6a0d21e21ff64d955c8fe83f9f6742a07eabd7ad
hanisha <hanisha946@gmail.com>
2017-11-29 12:51:32 +0530

    Hanisha | Removing patient image on anonymization

--- api/src/main/java/org/openmrs/module/anonymize/advice/PatientSaveInterceptor.java
+++ api/src/main/java/org/openmrs/module/anonymize/advice/PatientSaveInterceptor.java
@@ -1,10 +1,13 @@
 package org.openmrs.module.anonymize.advice;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.RandomStringUtils;
 import org.openmrs.Patient;
 import org.openmrs.PersonName;
+import org.openmrs.api.context.Context;
 import org.springframework.aop.MethodBeforeAdvice;
 
+import java.io.File;
 import java.lang.reflect.Method;
 
 public class PatientSaveInterceptor implements MethodBeforeAdvice {
@@ -22,9 +25,11 @@ public class PatientSaveInterceptor implements MethodBeforeAdvice {
                     personName.setFamilyName(RandomStringUtils.randomAlphabetic(5));
                     personName.setGivenName(RandomStringUtils.randomAlphabetic(5));
                     personName.setMiddleName(RandomStringUtils.randomAlphabetic(5));
+                    String imagesDirectory = Context.getAdministrationService().getGlobalProperty("emr.personImagesDirectory");
+                    if(patient.getUuid() != null && imagesDirectory != null)
+                        FileUtils.deleteQuietly(new File(imagesDirectory + "/" + patient.getUuid() + ".jpeg"));
                 }
             }
-
         }
     }
 }
